304189d5f2f3e0be1a78e1da9d08dd328a3917b1
tushar <tusharthakre800@gmail.com>
2024-09-16 23:30:04 +0530

    backend project

---
+++ app.js
@@ -0,0 +1,151 @@
+const express = require("express")
+const app = express()
+const postmodel =require ("./model/post")
+const usermodel =require ("./model/user")
+
+const cookieparser= require("cookie-parser")
+const bcrypt= require("bcrypt")
+const jwt =require("jsonwebtoken")
+const crypto = require("crypto")
+const path = require("path")
+const multer = require("multer")
+const post = require("./model/post")
+const upload = require("./config/multer")
+const { log } = require("console")
+
+
+app.set("view engine","ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"public")))
+app.use(cookieparser())
+
+
+
+
+app.get("/",(req,res)=>{
+res.render("index")
+})
+app.get("/profile/upload",(req,res)=>{
+res.render("profileupload")
+})
+app.post("/upload",isloggedin,upload.single("image"),async (req,res)=>{
+       const user  = await usermodel.findOne({email:req.user.email})
+       user.profilepic = req.file.filename;
+       await user.save()
+        console.log(req.file);
+       res.redirect("/profile")
+    
+    })
+
+
+app.get("/login" , (req,res)=>{
+res.render("login")
+})
+app.get("/profile", isloggedin, async(req,res)=>{
+   const user =  await usermodel.findOne({email:req.user.email}).populate("post")
+  
+//    console.log(user);
+
+    res.render("profil",{user})
+})
+app.get("/like/:id", isloggedin, async(req,res)=>{
+   const post =  await postmodel.findOne({_id: req.params.id}).populate("user")
+//    console.log(user);
+   if(post.likes.indexOf(req.user.userid) === -1){
+       post.likes.push(req.user.userid)
+   }
+   else{
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1)
+   }
+    await post.save();
+    res.redirect("/profile");
+})
+app.get("/edit/:id", isloggedin, async(req,res)=>{
+   const post =  await postmodel.findOne({_id: req.params.id}).populate("user")
+
+   res.render("edit",{post})
+  
+})
+app.post("/update/:id", isloggedin, async(req,res)=>{
+   const post =  await postmodel.findOneAndUpdate({_id: req.params.id},{content : req.body.content})
+
+   res.redirect("/profile")
+  
+})
+app.post("/post", isloggedin, async(req,res)=>{
+   const user =  await usermodel.findOne({email:req.user.email})
+   const{content}=req.body
+   const post =await postmodel.create({
+    user:user._id,
+    content
+   })
+   user.post.push(post._id);
+   await user.save();
+   res.redirect("/profile")
+})
+app.post("/register",async (req,res)=>{
+    const{email,username,name,password,age}=req.body
+    const user =  await usermodel.findOne({email})
+    if(user) return res.status(500).send("user already resiteger");
+    
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt, async (err,hash)=>{
+               const user = await usermodel.create({
+                    username,
+                    name,
+                    email,
+                    password:hash,
+                    age
+                })
+                const token = jwt.sign({email:email,userid:user._id},"tushar")
+                res.cookie("token", token);
+                res.redirect("/login")
+        })
+        
+    })
+})
+app.post("/login",async (req,res)=>{
+    const{email,password}=req.body
+    const user =  await usermodel.findOne({email})
+    if(!user) return res.status(500).send("something went wrong");
+    
+    bcrypt.compare(password,user.password,(err,result)=>{
+            if(result){
+                const token = jwt.sign({email:email,userid:user._id},"tushar")
+                res.cookie("token",token)    
+                res.status(200).redirect("/profile")
+            }
+                else res.redirect("/login")
+    })
+        
+    
+})
+
+
+app.get("/logout",(req,res)=>{
+    res.cookie("token","")
+    res.redirect("/login")
+
+    })
+
+
+    function isloggedin (req,res,next){
+        if(req.cookies.token==="") res.redirect("login")
+        else{
+    const data = jwt.verify(req.cookies.token,"tushar")
+    req.user =  data
+    next()
+}
+    }
+
+console.log(process.env.myname);
+
+const PORT =  process.env.PORT || 3000
+
+
+app.listen(PORT,()=>{
+    console.log("bhai server start ho gya hai port 3000");
+    
+})
+
---
+++ model/post.js
@@ -0,0 +1,24 @@
+const { name } = require("ejs")
+const mongoose = require("mongoose")
+
+
+
+const  postSchema  = mongoose.Schema({
+    
+    
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref : "user"
+        },
+        Date:{
+            type:Date,
+            default:Date.now
+        },
+        content:String,
+        likes:[
+            {type:mongoose.Schema.Types.ObjectId,ref:"user"}
+        ]
+    
+})
+
+module.exports = mongoose.model("post", postSchema)
