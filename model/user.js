693ef339ff617784c954afdecdfe14240e37c025
tushar <tusharthakre800@gmail.com>
2024-10-03 22:54:59 +0530

    port change

--- model/user.js
+++ model/user.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.MONGODBPORT)
+mongoose.connect(`mongodb://127.0.0.1:27017/miniproject`)
 
 
 const useraSchema  = mongoose.Schema({
